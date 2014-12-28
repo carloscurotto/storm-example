f6661c340bd679be86c9be5dd9ba752445c75b51
Carlos Curotto <carloscurotto@gmail.com>
2014-12-28 12:39:23 -0300

    Implement storm word-count example

---
+++ src/main/java/ar/com/carloscurotto/storm/wordcount/SplitSentenceBolt.java
@@ -0,0 +1,38 @@
+package ar.com.carloscurotto.storm.wordcount;
+
+import java.util.Map;
+
+import backtype.storm.task.OutputCollector;
+import backtype.storm.task.TopologyContext;
+import backtype.storm.topology.OutputFieldsDeclarer;
+import backtype.storm.topology.base.BaseRichBolt;
+import backtype.storm.tuple.Fields;
+import backtype.storm.tuple.Tuple;
+import backtype.storm.tuple.Values;
+
+public class SplitSentenceBolt extends BaseRichBolt {
+
+    private static final long serialVersionUID = 1L;
+    
+    private OutputCollector collector;
+
+    @SuppressWarnings("rawtypes")
+    @Override
+    public void prepare(Map theConfiguration, TopologyContext theTopologyContext, OutputCollector theCollector) {
+        collector = theCollector;
+    }
+
+    @Override
+    public void execute(Tuple tuple) {
+        String sentence = tuple.getString(0);
+        for (String word : sentence.split(" ")) {
+            collector.emit(tuple, new Values(word));
+        }
+        collector.ack(tuple);
+    }
+
+    @Override
+    public void declareOutputFields(OutputFieldsDeclarer declarer) {
+        declarer.declare(new Fields("word"));
+    }
+}
