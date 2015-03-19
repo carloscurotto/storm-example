5f9ebbc9a9b4d1fc9959f7cf7a04ec778179510d
javier.losada <javier.losada@globant.com>
2015-03-19 16:17:10 -0300

    Phoenix integration tests work in progress

---
+++ src/main/java/com/connection/PhoenixExample.java
@@ -0,0 +1,190 @@
+package com.connection;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Properties;
+
+import javax.sql.DataSource;
+
+import ar.com.carloscurotto.storm.complex.model.UpdateRow;
+import ar.com.carloscurotto.storm.complex.topology.propagator.context.UpdatePropagatorContext;
+import ar.com.carloscurotto.storm.complex.topology.propagator.hbase.DataSourceQueryExecutor;
+import ar.com.carloscurotto.storm.complex.topology.propagator.hbase.HBaseUpdatePropagator;
+import ar.com.carloscurotto.storm.complex.topology.propagator.hbase.MainConfigForTest;
+import ar.com.carloscurotto.storm.complex.topology.propagator.hbase.QueryBuilder;
+import ar.com.carloscurotto.storm.complex.topology.propagator.hbase.QueryExecutor;
+import ar.com.carloscurotto.storm.complex.topology.propagator.hbase.TradeTableBuilder;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class PhoenixExample {
+    
+    static Logger logger = LoggerFactory.getLogger("com.connection.PhoenixExample");
+
+    public static void main(String[] args) throws SQLException {
+        
+        logger.debug("Starting the Phoenix-Hbase test");
+
+        try {
+            Class.forName("org.apache.phoenix.jdbc.PhoenixDriver");
+        } catch (ClassNotFoundException e) {
+            System.out.println("Where is your PhoenixDriver");
+            e.printStackTrace();
+            return;
+        }
+        Statement stmt = null;
+        ResultSet rset = null;
+
+        String conString = buildURL();
+
+        Properties props = getPropertiesConfigured();
+
+//        Connection con = new MainConfigForTest().getDataSource().getConnection();
+//        Connection con = DriverManager.getConnection(conString);
+        // Connection con = DriverManager.getConnection(conString, props);
+
+//        SimpleTest(props, conString);
+        
+        TestTradeTable();
+
+    }
+    
+    public static void TestTradeTable() throws SQLException {
+        
+        logger.debug("Getting the DataSource and the Conncetion from it");
+        DataSource theDataSource = new MainConfigForTest().getDataSource("");
+        
+        Connection con = theDataSource.getConnection();
+        
+        dropTradeTable(con);
+        createTradeTable(con);
+        
+        logger.debug("Calling HBaseUpdatePropagator and see what happens");
+        QueryBuilder theQueryBuilder = new QueryBuilder();
+        
+        QueryExecutor queryExecutor = new DataSourceQueryExecutor(theDataSource);
+        
+        HBaseUpdatePropagator hBaseUpdatePropagator = new HBaseUpdatePropagator(theQueryBuilder, queryExecutor);
+        
+        hBaseUpdatePropagator.open();
+        
+        UpdatePropagatorContext theContext = getUpdatePropagatorContext();
+        
+        hBaseUpdatePropagator.propagate(theContext );
+        
+        logger.debug("HBaseUpdatePropagator finished");
+        
+        con.close();
+    }
+
+    private static void dropTradeTable(Connection con) throws SQLException {
+        
+        logger.debug("Droping the TRADE table first");
+        
+        Statement stmt;
+        ResultSet rset;
+        stmt = con.createStatement();
+        
+        String sql = TradeTableBuilder.dropTable();
+        stmt.execute(sql);
+        con.commit();
+        
+        stmt.close();
+        
+    }
+
+    private static void createTradeTable(Connection con) throws SQLException {
+        logger.debug("Creating TRADE table");
+        Statement stmt;
+        ResultSet rset;
+        stmt = con.createStatement();
+        
+        String sql = TradeTableBuilder.createTable();
+        stmt.execute(sql);
+        con.commit();
+        
+        stmt.close();
+        
+        
+    }
+
+    private static void SimpleTest(Properties props, String conString) throws SQLException {
+        
+        Connection con = DriverManager.getConnection(conString);
+        
+        Statement stmt;
+        ResultSet rset;
+        stmt = con.createStatement();
+
+        stmt.executeUpdate("create table test (mykey integer not null primary key, mycolumn varchar)");
+        stmt.executeUpdate("upsert into test values (1,'Hello')");
+        stmt.executeUpdate("upsert into test values (2,'World!')");
+        con.commit();
+
+        PreparedStatement statement = con.prepareStatement("select * from test");
+        rset = statement.executeQuery();
+        while (rset.next()) {
+            System.out.println(rset.getString("mycolumn"));
+        }
+        statement.close();
+        
+        con.close();
+    }
+
+    private static String buildURL() {
+        // String conString = "jdbc:phoenix:192.168.56.101:2181";
+        // String conString = "jdbc:phoenix:127.0.0.1:2181";
+        String conString = "jdbc:phoenix:localhost:2181:/hbase";
+        // String conString = "jdbc:phoenix:sandbox.hortonworks.com";
+        return conString;
+    }
+
+    private static Properties getPropertiesConfigured() {
+        Properties props = new Properties();
+
+        props.setProperty("hbase.client.retries.number", "1");
+        props.setProperty("zookeeper.recovery.retry", "1");
+
+        // props.setProperty("user","fred");
+
+        props.setProperty("hbase.master", "master" + ":" + "60000");
+        props.setProperty("zookeeper.znode.parent", "/hbase-unsecure");
+        props.setProperty("hbase.zookeeper.quorum", "sandbox.hortonworks.com");
+        props.setProperty("hbase.zookeeper.property.clientPort", "2181");
+        return props;
+    }
+    
+    private static UpdatePropagatorContext getUpdatePropagatorContext() {
+        HashMap<String, Object> theParameters = new HashMap<String, Object>();
+        Map<String, Object> keyColumns = new HashMap<String, Object>();
+        
+        keyColumns.put("record_no", 1);
+//        keyColumns.put("condition1", "conditionValue1");
+//        keyColumns.put("condition2", "conditionValue2");
+        
+        String[] columnNames = TradeTableBuilder.createColumnNames();
+        Object[] columnValues = TradeTableBuilder.createColumnValues();
+        
+        Map<String, Object> updateColumns = new HashMap<String, Object>();
+        for (int i=0; i < columnNames.length; i++) {
+            updateColumns.put(columnNames[i], columnValues[i]);
+        }
+        
+//        updateColumns.put("column2", "value2");
+
+        UpdateRow theRow = new UpdateRow("theSystemId", keyColumns, updateColumns);
+
+        UpdatePropagatorContext updatePropagatorContext = new UpdatePropagatorContext("TRADE",
+                theRow, theParameters);
+        
+        return updatePropagatorContext;
+    }
+
+}
--- src/test/java/ar/com/carloscurotto/storm/complex/topology/propagator/hbase/HBaseUpdatePropagatorTest.java
+++ src/test/java/ar/com/carloscurotto/storm/complex/topology/propagator/hbase/HBaseUpdatePropagatorTest.java
@@ -10,7 +10,11 @@ import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertThat;
 import static org.junit.Assert.assertTrue;
 
+import java.sql.Connection;
 import java.sql.SQLException;
+import java.sql.Statement;
+
+import javax.sql.DataSource;
 
 import org.easymock.EasyMockRunner;
 import org.easymock.Mock;
@@ -49,9 +53,19 @@ public class HBaseUpdatePropagatorTest {
     @Mock
     private UpdatePropagatorContext context;
 
+    @Mock
+    private DataSource dataSourceMock;
+
+    @Mock
+    private Connection connectionMock;
+
+    @Mock
+    private Statement statementMock;
+
     @Before
     public void setUp() {
-        hbaseInternalUpdatePropagator = new HBaseUpdatePropagator(queryBuilderMock, queryExecutorMock);
+        hbaseInternalUpdatePropagator = new HBaseUpdatePropagator(queryBuilderMock,
+                queryExecutorMock);
     }
 
     @Test(expected = NullPointerException.class)
@@ -65,7 +79,35 @@ public class HBaseUpdatePropagatorTest {
     }
 
     @Test
-    public void propagateNullUpdatePropagatorContext() {
+    public void executeWithNonNullUpdateAndUpdatePropagatorResultIsSuccess() throws SQLException {
+        ReflectionTestUtils.setField(hbaseInternalUpdatePropagator, "isOpen", true);
+        expect(queryBuilderMock.build(updatePropagatorContextMock)).andReturn("upsertQuery");
+        queryExecutorMock.execute("upsertQuery");
+        expectLastCall();
+        replay(updatePropagatorContextMock, queryBuilderMock, queryExecutorMock);
+        UpdatePropagatorResult updatePropagatorResult = hbaseInternalUpdatePropagator
+                .propagate(updatePropagatorContextMock);
+        assertThat(updatePropagatorResult.getStatus(), equalTo(ResultStatus.SUCCESS));
+        assertThat(updatePropagatorResult.getMessage(), equalTo("Row succefully updated."));
+        verify(updatePropagatorContextMock, queryBuilderMock, queryExecutorMock);
+    }
+
+    @Test
+    public void executeWithNonNullUpdateAndUpdatePropagatorResultIsNotSuccess() throws SQLException {
+        ReflectionTestUtils.setField(hbaseInternalUpdatePropagator, "isOpen", true);
+        expect(queryBuilderMock.build(updatePropagatorContextMock)).andReturn("upsertQuery");
+        queryExecutorMock.execute("upsertQuery");
+        expectLastCall().andThrow(new RuntimeException("An error occurred"));
+        replay(updatePropagatorContextMock, queryBuilderMock, queryExecutorMock);
+        UpdatePropagatorResult updatePropagatorResult = hbaseInternalUpdatePropagator
+                .propagate(updatePropagatorContextMock);
+        assertThat(updatePropagatorResult.getStatus(), equalTo(ResultStatus.FAILURE));
+        assertThat(updatePropagatorResult.getMessage(), equalTo("An error occurred"));
+        verify(updatePropagatorContextMock, queryBuilderMock, queryExecutorMock);
+    }
+
+    @Test
+    public void doExecuteNullUpdatePropagatorContext() {
         thrown.expect(NullPointerException.class);
         thrown.expectMessage(equalTo("The Context cannot be null."));
         hbaseInternalUpdatePropagator.open();
@@ -103,7 +145,8 @@ public class HBaseUpdatePropagatorTest {
     public void open() throws SQLException {
         ReflectionTestUtils.setField(hbaseInternalUpdatePropagator, "isOpen", false);
         hbaseInternalUpdatePropagator.open();
-        boolean isOpen = (Boolean) ReflectionTestUtils.getField(hbaseInternalUpdatePropagator, "isOpen");
+        boolean isOpen = (Boolean) ReflectionTestUtils.getField(hbaseInternalUpdatePropagator,
+                "isOpen");
         assertTrue(isOpen);
     }
 
