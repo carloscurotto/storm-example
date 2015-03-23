ebbc83e8d18ab972caa5d45e950615fa702418a9
Carlos Curotto <carloscurotto@gmail.com>
2015-03-23 15:11:27 -0300

    wip

---
+++ src/main/java/ar/com/carloscurotto/storm/complex/transport/activemq/ActiveMQResultProducer.java
@@ -0,0 +1,80 @@
+package ar.com.carloscurotto.storm.complex.transport.activemq;
+
+import java.io.Serializable;
+
+import javax.jms.Connection;
+import javax.jms.DeliveryMode;
+import javax.jms.Destination;
+import javax.jms.MessageProducer;
+import javax.jms.Session;
+
+import org.apache.activemq.ActiveMQConnectionFactory;
+import org.apache.commons.lang3.Validate;
+
+import ar.com.carloscurotto.storm.complex.model.Result;
+import ar.com.carloscurotto.storm.complex.service.OpenAwareBean;
+import ar.com.carloscurotto.storm.complex.transport.ResultProducer;
+
+public class ActiveMQResultProducer extends OpenAwareBean<Result, Void> implements ResultProducer, Serializable {
+
+    private static final long serialVersionUID = 1L;
+    
+    private String brokerUrl;
+    private Connection connection;
+    private Session session;
+    private Destination destination;
+    private MessageProducer producer;
+    
+    public ActiveMQResultProducer(final String theBrokerUrl) {
+        Validate.notBlank(theBrokerUrl, "The broker url can not be blank.");
+        brokerUrl = theBrokerUrl;
+    }
+
+    @Override
+    public void send(Result theResult) {
+        doExecute(theResult);
+    }
+
+    @Override
+    protected void doOpen() {
+        try {
+            ActiveMQConnectionFactory connectionFactory = new ActiveMQConnectionFactory(brokerUrl);
+            connection = connectionFactory.createConnection();
+            connection.start();
+            session = connection.createSession(false, Session.AUTO_ACKNOWLEDGE);
+            destination = session.createTopic("test");
+            producer = session.createProducer(destination);
+            producer.setDeliveryMode(DeliveryMode.NON_PERSISTENT);            
+        } catch (Exception e) {
+            throw new RuntimeException("Error creating active mq producer.", e);
+        }
+        
+    }
+
+    @Override
+    protected void doClose() {
+        try {
+            if (session != null) {
+                session.close();
+            }            
+            if (connection != null) {
+                connection.close();
+            }
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        } finally {
+            session = null;
+            connection = null;
+            destination = null;
+            producer = null;
+        }
+    }
+
+    @Override
+    protected Void doExecute(Result theResult) {
+        return null;
+    }
+    
+    
+    
+}
