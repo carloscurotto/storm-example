4f27465538e03b2d1781d02a57a19344d7d34c7f
javier.losada <javier.losada@globant.com>
2015-03-25 18:02:56 -0300

    Phoenix QueryBuilder correction in progress

--- src/test/java/ar/com/carloscurotto/storm/complex/topology/propagator/hbase/TradeTableBuilder.java
+++ src/test/java/ar/com/carloscurotto/storm/complex/topology/propagator/hbase/TradeTableBuilder.java
@@ -22,6 +22,6 @@ public class TradeTableBuilder {
     }
 
     public static String selectAllTradeTable() {
-        return "SELECT * FROM TRADE LIMIT 1";
+        return "SELECT * FROM TRADE";
     }
 }
